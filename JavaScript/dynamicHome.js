6c8ab3e17f21950345f72d955e744ed162ef22d2
Ahmed Dafalla <nmawg22@gmail.com>
2024-04-21 13:41:50 +0200

    F

--- JavaScript/dynamicHome.js
+++ JavaScript/dynamicHome.js
@@ -1,108 +1,61 @@
-// Show Books in index file 
+/// this function will be used in the home page 
+/// you will add new books about 4 to 6 books in the single category
+/// delete the description  and category from  the array 
 
-const FictionBooks = [
+
+const books = [
     {
+        title: "The Great Gatsby",
         cover: "../img/images.jpg",
-        borrowLink: "../html/sualBook.html"
+        borrowLink: "../html/book.html"
     },
     {
+        title: "Enceladus",
         cover: "../img/Enceladus.jpg",
         borrowLink: "../html/EnceladusBook.html"
     },
-];
-
-const ProgrammingBooks = [
     {
+        title: "C Programming",
         cover: "../img/C_prog.jpg",
         borrowLink: "../html/C_progBook.html"
     },
     {
-        cover: "../img/clean_code.jpg",
-        borrowLink: "../html/clean_codeBook.html"
-    },
-];
-
-const ChildrenBooks = [
-    {
+        title: "Kindness is my Superpower",
         cover: "../img/Kindness_is_my_Power.jpg",
         borrowLink: "../html/Kindness_is_my_PowerBook.html"
     },
     {
+        title: "The Cat in the Hat",
         cover: "../img/The_Cat_in_the_hat.jpg",
         borrowLink: "../html/The_Cat_in_the_hatBook.html"
     },
+    {
+        title: "Clean Code",
+       cover: "../img/clean_code.jpg",
+        borrowLink: "../html/clean_codeBook.html"
+    },
+    // Add more books later
 ];
 
-function displayFictionBooks() {
-    const container = document.getElementById("FictionBooks");
-    container.innerHTML = "";
-    
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book-details");
-    
-    const bookP = document.createElement("p");
-    bookP.textContent = "Fiction Books";
-    bookP.classList.add("book-category");
 
-    FictionBooks.forEach(book => {
-        bookDiv.innerHTML += `
-            <img src="${book.cover}" alt="Book Cover" class="book-cover" display>
-            <div class="details">
-                <a href="${book.borrowLink}" class="show-details-button">Show details</a>
-            </div>
-        `;
-    });
-    container.appendChild(bookP);
-    container.appendChild(bookDiv);
-}
-
-function displayProgrammingBooks() {
-    const container = document.getElementById("ProgrammingBooks");
-    container.innerHTML = "";
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book-details");
-
-    const bookP = document.createElement("p");
-    bookP.textContent = "Programming Books";
-    bookP.classList.add("book-category");
-    
-    ProgrammingBooks.forEach(book => {
-        bookDiv.innerHTML += `
+function displayBooks() {
+    const container = document.getElementById("bookContainer");
+    container.innerHTML = ""; // Clear previous content
+    books.forEach(book => {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book-details");
+        bookDiv.innerHTML = `
             <img src="${book.cover}" alt="Book Cover" class="book-cover">
             <div class="details">
-                <a href="${book.borrowLink}" class="show-details-button">Show details</a>
+                
+                <p><strong>title:</strong> ${book.title}</p>
+                <br>
+                <a href="${book.borrowLink}" class="borrow-button">Show details</a>
             </div>
         `;
+        container.appendChild(bookDiv);
     });
-    container.appendChild(bookP);
-    container.appendChild(bookDiv);
 }
 
-function displayChildrenBooks() {
-    const container = document.getElementById("ChildrenBooks");
-    container.innerHTML = "";
-    
-    const bookDiv = document.createElement("div");
-    bookDiv.classList.add("book-details");
-    
-    const bookP = document.createElement("p");
-    bookP.textContent = "Children Books";
-    bookP.classList.add("book-category");
-    
-    ChildrenBooks.forEach(book => {
-        bookDiv.innerHTML += `
-            <img src="${book.cover}" alt="Book Cover" class="book-cover">
-            <div class="details">
-                <a href="${book.borrowLink}" class="show-details-button">Show details</a>
-            </div>
-        `;
-    });
-    container.appendChild(bookP);
-    container.appendChild(bookDiv);
-}
-
-window.onload = function() {
-    displayFictionBooks();
-    displayProgrammingBooks();
-    displayChildrenBooks();
-};
+// Call the function to display books when the page loads
+window.onload = displayBooks;
