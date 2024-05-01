d9911eee6c65b57a88699bb169afa72d9a5960d6
Ahmed Dafalla <nmawg22@gmail.com>
2024-05-01 08:25:04 +0300

    SERVER been created

---
+++ python/view.py
@@ -0,0 +1,7 @@
+#A view is a function or method that takes http requests as arguments, 
+#imports the relevant model(s), and finds out what data to send to the template, and returns the final result.
+
+#he views are usually located in a file called views.py.
+
+
+print(10 + 101)
