70f04eef9a38917dc2a0cc9a3f4919a3f63ff750
Ahmed Dafalla <nmawg22@gmail.com>
2024-05-01 09:45:24 +0300

    create index.html in Django

--- my_library/members/views.py
+++ my_library/members/views.py
@@ -2,8 +2,9 @@
 
 # Create your views here.
 
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
