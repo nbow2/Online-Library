d43906badc064f92ba0e9fa5f701c298c3975296
Ahmed Dafalla <nmawg22@gmail.com>
2024-05-20 14:44:48 +0300

    added property email for profile Done

--- my_library/members/models.py
+++ my_library/members/models.py
@@ -17,7 +17,10 @@ class Profile(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"profile by {self.user} on {self.name}"
+        return f"profile by {self.user} is {self.name} "
+    
+    def email(self):
+        return self.userType.email
 
 
 
